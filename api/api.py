76179d76dab0905ec7ae2feb54fa3893778195c3
Rikaiyah Winters <rikaiyahswinters@gmail.com>
2025-03-16 20:42:31 -0500

    Added update and delete functions; also added two blogs

--- api/api.py
+++ api/api.py
@@ -55,5 +55,45 @@ def add_blog():
     }
     return jsonify({'message': 'Blog added successfully', 'blog': new_blog_data})
 
+@app.route('/api/blogs/<int:blog_id>', methods=['PUT'])
+def update_blog(blog_id):
+    blog = Blog.query.get(blog_id)
+    if not blog:
+        return jsonify({'error': 'Blog not found'}), 404
+    data = request.get_json()
+
+    # Validate the incoming JSON data for required fields
+    required_fields = ['thumbnail', 'title', 'description', 'category']
+
+    for field in required_fields:
+        if field not in data or data[field] == "":
+            return jsonify({'error': f"Missing required field: '{field}'"}), 400
+
+    blog.thumbnail = data['thumbnail']
+    blog.title = data['title']
+    blog.description = data['description']
+    blog.catagory = data['category']
+
+    db.session.commit()
+ # Serialize the updated recipe and return it as JSON
+    updated_blog = {
+        'id': blog.id,
+        'thumbnail': blog.thumbnail,
+        'title': blog.title,
+        'description': blog.description,
+        'category': blog.catagory
+    }
+
+    return jsonify({'message': 'Blog updated successfully', 'blog': updated_blog})
+
+@app.route('/api/blogs/<int:blog_id>', methods=['DELETE'])
+def delete_blog(blog_id):
+    blog = Blog.query.get(blog_id)
+    if not blog:
+        return jsonify({'error': 'Blog not found'}), 404
+    db.session.delete(blog)
+    db.session.commit()
+    return jsonify({'message': 'Blog deleted successfully'})
+
 if __name__ == '__main__':
     app.run(debug=True)
