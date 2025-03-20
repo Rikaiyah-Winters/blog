a1f2c54c22f73d2fc18a14835289056056a27ada
Rikaiyah Winters <rikaiyahswinters@gmail.com>
2025-03-19 20:24:55 -0500

    Deleted unneseccary sections from BlogFull component

--- src/components/BlogFull.js
+++ src/components/BlogFull.js
@@ -21,16 +21,7 @@ const BlogFull = () => {
                 <h3>Description:</h3>
                 <p>{ }</p>
 
-                <h3>Ingredients:</h3>
-
-                <ul className='ingredient-list'>
-
-                </ul>
-                <h3>Instructions:</h3>
-
-                <pre className='formatted-text'>{ }</pre>
-
-                <h3>Servings: { }</h3>
+                {/*Consider adding a "Contributer" section */}
             </article>
         </div>
     )
